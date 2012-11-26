de6087dbe53b12188a055f0f4193753407f25ab1
Peter <Peter@10.140.99.26>
2012-11-26 11:26:29 +0100

    areal

---
+++ areal/src/areal.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : areal.c
+ Author      : Peter Olsen
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int radius, area;
+
+int main(void) {
+
+	printf("Enter radius(i.e. 10):");
+	fflush(stdout);
+	scanf("%d", &radius);
+	area = (int) (3.14159 * radius * radius);
+	printf("\n\nArea = %d\n", area);
+	fflush(stdout);
+
+	return 0;
+}
