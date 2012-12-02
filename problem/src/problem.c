75a8c3bbb3f4a595a93499ce876c5073ff729fc0
Peter <Peter@Overdrevet2>
2012-12-02 21:33:32 +0100

    Et lille program med en fejl mindst som jeg har løst.

---
+++ problem/src/problem.c
@@ -0,0 +1,27 @@
+/*
+ * problem.c
+ *
+ *  Created on: 02/12/2012
+ *      Author: Peter
+ */
+/*Et problem i programmet*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+
+
+{
+	printf(" Bliv ved\n med at lede\n");
+	printf(" Du\n vil finde den\n");
+
+
+	return 0;
+}
+
+
+
